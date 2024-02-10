d5dde6248b114a300cd07fb32c8b4feb4e24db36
airqx <159660071+airqx@users.noreply.github.com>
2024-02-11 01:20:29 +0300

    Update Main.java
    comments to explain the logic has been added

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         int[] array = {5, 2, 8, 12, 1, 6, 3, 9};
@@ -15,16 +13,17 @@ public class Main {
         System.out.print("Sorted array: ");
         printArray(array);
     }
-
+// insertionSort for array 
     public static void insertionSort(int[] arr) {
         int n = arr.length;
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
-
+// Shift elements of arr[0..i-1] that are greater than key, to one position ahead of their current position
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
+// Insert the key element into its correct position in the sorted part of the array
             }
             arr[j + 1] = key;
         }
