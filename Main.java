d1932c5e3e3a6be081ed6053a7d1ce1b5e30d812
airqx <159660071+airqx@users.noreply.github.com>
2024-02-11 00:56:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 8, 12, 1, 6, 3, 9};
+
+        System.out.print("Original array: ");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.print("Sorted array: ");
+        printArray(array);
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
