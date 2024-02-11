90c27133aa6737b75f187bcc58ba4c9c368717c0
Euph369 <Alshuraim20@gmail.com>
2024-02-11 18:53:15 +0300

    Most updated

---
+++ Sorter.java
@@ -0,0 +1,29 @@
+public class Sorter {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j; //searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test the selection sort method
+        int[] testArray = {6804, 34, 555, 12, 22, 11, 93};
+        selectionSort(testArray);
+        for (int i : testArray) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // New sort method that calls previous selection sort
+    public static void sort(int[] array) {
+        selectionSort(array);
+    }
+}
