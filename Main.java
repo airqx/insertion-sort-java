9236816e2c131f2d9a52db1c8022bee227e2ed8d
Euph369 <alshuraim20@gmail.com>
2024-02-11 21:17:54 +0300

    Most updated after separating each method and accessing them by the main

--- Main.java
+++ Main.java
@@ -1,41 +1,21 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int[] array = {5, 2, 8, 12, 1, 6, 3, 9};
 
         System.out.print("Original array: ");
-        printArray(array);
-// creating sort function to call the insertionSort function
-        sort(array);
+        InsertionSort.printArray(array);
 
+        InsertionSort.sort(array);
         System.out.print("Sorted array: ");
-        printArray(array);
-    }
-    public static void sort(int[] array) {
-        insertionSort(array);
-        } 
-// insertionSort for array 
-    public static void insertionSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
-// Shift elements of arr[0..i-1] that are greater than key, to one position ahead of their current position
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-// Insert the key element into its correct position in the sorted part of the array
-            }
-            arr[j + 1] = key;
-        }
-    }
+        InsertionSort.printArray(array);
 
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; ++i) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+        int[] testArray = {6804, 34, 555, 12, 22, 11, 93};
+
+        System.out.print("Before sort: ");
+        Sorter.printArray(testArray);
+
+        Sorter.sort(testArray) ;
+        System.out.print("After sort: ");
+        Sorter.printArray(testArray);
     }
 }
